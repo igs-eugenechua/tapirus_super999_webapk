82d2684d275bd43539dce68698d698bffc43fc02
eugenechua <eugenechua@igs.com.tw>
2024-08-12 17:22:55 +0800

    新版

--- js/i18n.js
+++ js/i18n.js
@@ -12,6 +12,12 @@ const messages = {
       ranking: "Rank",
       playerNickname: "Player nickname",
       activityScore: "Event Record",
+    },
+    tutorial: {
+      step1: 'Step 1. Click to download APK',
+      step2: 'Step 2. Different browsers, different download prompts will appear. Please click [Keep] or [Download]',
+      step3: 'Step 3. Go to download file in phone, and click it',
+      step4: 'Step 4. After that,  click Install to complete the process'
     }
   },
   zh_Hans: {
@@ -27,7 +33,13 @@ const messages = {
       ranking: "名次",
       playerNickname: "玩家昵称",
       activityScore: "活动积分",      
-    }
+    },
+    tutorial: {
+      step1: '步骤1. 点击APK下载',
+      step2: '步骤2. 依手机品牌或浏览器会跳出不同提示，点选下载/保留',
+      step3: '步骤3. 前往手机下载档案存放位置点选档案',
+      step4: '步骤4. 再点选安装即可完成游戏安装'
+    }    
   }
 }
 
